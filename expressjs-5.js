cb5e82547418346f7686d7068a671a3ea04a296e
ayush754dutta <143083926+ayush754dutta@users.noreply.github.com>
2023-10-26 00:13:14 +0530

    Update expressjs-5.js

--- expressjs-5.js
+++ expressjs-5.js
@@ -5,3 +5,80 @@
 const express = require("express");
 
 const app = express();
+
+// 1. .listen():
+app.listen(5500, () => {
+  console.log("Server is listening to the port 5500");
+});
+
+// 2. .get():
+
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Welcome to Home Page!</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).send("<h1>About Page</h1>");
+});
+
+app.all("*", (req, res) => {
+  res.status(404);
+  res.send("<h1>Page not found</h1>");
+});
+
+////////////////////////////////////////////////////////////////////////
+
+// Express-App Example:
+
+const express = require("express");
+
+const navbar = express();
+
+const path = require("path");
+
+navbar.use(express.static("./public"));
+
+navbar.get("/", (req, res) => {
+  res.sendFile(
+    path.resolve(
+      __dirname,
+      "./node-express-course/02-express-tutorial/navbar-app/index.html"
+    )
+  );
+});
+
+navbar.all("*", (req, res) => {
+  res.status(404).send("resource not found");
+});
+
+navbar.listen(5500, () => {
+  console.log("Server is listening to port 5500...");
+});
+
+
+//////////////////////////////////////////////////////////////////
+
+// Express-All Static
+
+const express = require("express");
+
+const navbar = express();
+
+navbar.use(express.static("./public"));
+
+// navbar.get("/", (req, res) => {
+//   res.sendFile(
+//     path.resolve(
+//       __dirname,
+//       "./node-express-course/02-express-tutorial/navbar-app/index.html"
+//     )
+//   );
+// });
+
+navbar.all("*", (req, res) => {
+  res.status(404).send("resource not found");
+});
+
+navbar.listen(5500, () => {
+  console.log("Server is listening to port 5500...");
+});
