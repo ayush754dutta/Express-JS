d1a6fc5767f269ae1dda76423f44fa75123ceb67
ayush754dutta <143083926+ayush754dutta@users.noreply.github.com>
2023-11-01 17:02:58 +0530

    Update json-6.js

--- json-6.js
+++ json-6.js
@@ -163,3 +163,36 @@ app.get("/api/v1/query", (req, res) => {
 app.listen(7000, () => {
   console.log("Listening to port 7000...");
 });
+
+
+////////////////////////////////////////////////////////////////////
+
+// Middleware Set-up:
+
+const express = require("express");
+
+const app = express();
+
+const logger = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  console.log(method, url, time);
+  // return res.send("<h1>Testing</h1>");
+  next();
+};
+
+app.get("/", logger, (req, res) => {
+  // const method = req.method;
+  // const url = req.url;
+  // const time = new Date().getFullYear();
+  // console.log(method, url, time);
+  res.send("Home");
+});
+
+app.get("/about", logger, (req, res) => {
+  res.send("<h1>About</h1>");
+});
+app.listen(7000, () => {
+  console.log("Listening to port 7000...");
+});
