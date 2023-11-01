2fc0e2545957d026a3de8819e4904af2e4a87153
ayush754dutta <143083926+ayush754dutta@users.noreply.github.com>
2023-11-01 17:34:46 +0530

    Update json-6.js

--- json-6.js
+++ json-6.js
@@ -196,3 +196,36 @@ app.get("/about", logger, (req, res) => {
 app.listen(7000, () => {
   console.log("Listening to port 7000...");
 });
+
+
+///////////////////////////////////////////////////////////
+
+// App.Use():
+
+const express = require("express");
+
+const app = express();
+
+const logger = require("./app-use-logger");
+
+app.use(logger);
+
+app.get("/", (req, res) => {
+  res.send("Home");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>About</h1>");
+});
+
+app.get("/api/products", (req, res) => {
+  res.send("Products");
+});
+
+app.get("/api/items", (req, res) => {
+  res.send("<h1>Data</h1>");
+});
+
+app.listen(7000, () => {
+  console.log("Listening to port 7000...");
+});
