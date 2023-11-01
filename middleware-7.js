9b5782bff3d7cdb4c4a7d0c527bcd3da8d00c377
ayush754dutta <143083926+ayush754dutta@users.noreply.github.com>
2023-11-01 18:16:03 +0530

    Update middleware-7.js

--- middleware-7.js
+++ middleware-7.js
@@ -65,3 +65,40 @@ app.get("/api/items", (req, res) => {
 app.listen(7000, () => {
   console.log("Listening to port 7000...");
 });
+
+
+//////////////////////////////////////////////////////////
+
+// Multiple middleware functions:
+
+const express = require("express");
+
+const app = express();
+
+const logger = require("./app-use-logger");
+
+const authorize = require("./app-use-authorize");
+
+app.use([authorize, logger]);
+
+app.get("/", (req, res) => {
+  res.send("Home");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>About</h1>");
+});
+
+app.get("/api/products", (req, res) => {
+  res.send("Products");
+});
+
+app.get("/api/items", (req, res) => {
+  console.log(req.user);
+  res.send("<h1>Data</h1>");
+});
+
+app.listen(7000, () => {
+  console.log("Listening to port 7000...");
+});
+
